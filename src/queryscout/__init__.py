07e53453c2068d7430957dd5579cb75e4073210e
Malthegudum <82721293+Malthegudum@users.noreply.github.com>
2026-08-19 12:37:49 +0200

    Simplify package exports

--- src/queryscout/__init__.py
+++ src/queryscout/__init__.py
@@ -1,6 +1,1 @@
-"""QueryScout: an extensible MCP server for statistical data APIs."""
-
-from .registry import get_source, get_sources
-from .source import SourceSpec, ToolSpec
-
-__all__ = ["SourceSpec", "ToolSpec", "get_source", "get_sources"]
+"""QueryScout MCP server."""
